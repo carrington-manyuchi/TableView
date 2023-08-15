c8bc81a8980c404225db621a905c9ffc11d2811d
Carrington <Manyuchi>
2023-08-15 12:00:59 +0200

    Added CustomCell

---
+++ TableView/CustomCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomCell.swift
+//  TableView
+//
+//  Created by DA MAC M1 157 on 2023/08/15.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    
+    static let identifier = "CustomCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -11,19 +11,61 @@ class ViewController: UIViewController {
     
     //MARK: - Variables
     private let image: [UIImage] = [
-        UIImage(named: "caar")!
+        UIImage(named: "1")!
     ]
     
     
     //MARK: - UI Componets
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .systemBackground
+        tableView.allowsSelection = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableView
+    }()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-
+        setupUI()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     //MARK: - Setup
 
+    private func setupUI(){
+        view.backgroundColor = .blue
+        view.addSubview(tableView)
+        configureConstraints()
+    }
+    
+    
+    private func configureConstraints() {
+        
+        let composeTableview = [
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(composeTableview)
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Carring ton"
+        return cell
+        
+    }
+    
+    
+}
